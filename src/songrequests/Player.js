fcbab2115c7e12c64b8898f9216dc3fcd613c911
MergHQ <hugo.holm@live.com>
2016-06-22 15:22:02 +0300

    Reformats Player.js

--- src/songrequests/Player.js
+++ src/songrequests/Player.js
@@ -1,31 +1,31 @@
 var ytdl = require('ytdl-core');
 
 function main(url) {
-	var currentStream = null;
-	var audioContext = App.botClient.getAudioContext();
-	var onEndFunc = null;
-	
-	this.setOnEnd = function(callback) {
-		onEndFunc = callback;
-	};
-	
-	var ytdlStream = null;
-	try {
-		ytdlStream = ytdl(url, {filter: 'audioonly'});
-	} catch(e) { console.log(e); }
-	
-	ytdlStream.on('end', function() {
-		onEndFunc();
-	});
-	
-	// So discord.js encodes the data stream for me with ffmpeg. Sweet!
-	audioContext.playRawStream(ytdlStream, {volume: 0.1});
+  var currentStream = null;
+  var audioContext = App.botClient.getAudioContext();
+  var onEndFunc = null;
 
-	this.release = function() {
-		ytdlStream.end();
-		audioContext.stopPlaying();
-		if(ytdlStream.destroy) ytdlStream.destroy();
-	};
+  this.setOnEnd = function (callback) {
+    onEndFunc = callback;
+  };
+
+  var ytdlStream = null;
+  try {
+    ytdlStream = ytdl(url, { filter: 'audioonly' });
+  } catch (e) { console.log(e); }
+
+  ytdlStream.on('end', function () {
+    onEndFunc();
+  });
+
+  // So discord.js encodes the data stream for me with ffmpeg. Sweet!
+  audioContext.playRawStream(ytdlStream, { volume: 0.1 });
+
+  this.release = function () {
+    ytdlStream.end();
+    audioContext.stopPlaying();
+    if (ytdlStream.destroy) ytdlStream.destroy();
+  };
 }
 
 module.exports = main;
