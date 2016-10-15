f8d34c5054db39847b5804bb1ff9a32be2807fec
MergHQ <hugo.holm@live.com>
2016-10-15 15:26:31 +0300

    Fix voice crash

--- src/QueuePlayer.js
+++ src/QueuePlayer.js
@@ -19,26 +19,15 @@ module.exports = function () {
   function play() {
     if (self.isPlaying) return;
     var current = self.queue.shift();
-    var connection = App.Client.voiceConnections.find(c => c.channelID === current.voiceChannelId);
-    if (!connection) {
-      App.Client.joinVoiceChannel(current.voiceChannelId).then(con => {
-        self.isPlaying = true;
-        self.voiceConnection = con;
-        con.play(ytdl(current.data.url, { audioonly: true }));
-        con.on('end', () => {
-          self.isPlaying = false;
-          if (self.queue.length !== 0)
-            play();
-        });
-      });
-    } else {
+    App.Client.joinVoiceChannel(current.voiceChannelId).then(con => {
       self.isPlaying = true;
-      connection.play(ytdl(current.data.url, { audioonly: true }));
-      connection.on('end', () => {
+      self.voiceConnection = con;
+      con.play(ytdl(current.data.url, { audioonly: true }));
+      con.on('end', () => {
         self.isPlaying = false;
         if (self.queue.length !== 0)
           play();
       });
-    }
+    });
   }
 };
